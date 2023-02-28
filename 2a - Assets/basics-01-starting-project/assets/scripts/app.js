82774b727fe729bec124e0412e33979f9d44d548
Kyle Czernuszka <czernuka@outlook.com>
2023-02-28 07:41:39 -0500

    commit

--- 2a - Assets/basics-01-starting-project/assets/scripts/app.js
+++ 2a - Assets/basics-01-starting-project/assets/scripts/app.js
@@ -1,26 +1,75 @@
 
-// 2a
-// goal - create calculator
-// initialize (set value) of a constant (that cannot change)
+// initialize constant
 const defaultResult = 0;
 // declare variable
 let currentResult = defaultResult;
-// set and overwrite variable to the initial value plus 10; information on the right-side of = is evaluated first
-//// currentResult = currentResult + 10;
-//// currentDescription = `We start with ${defaultResult} and \n now we have:`;
-    // *to see the line break (\n) on the webpage, go to Developer tools > add 'white-space: pre;' under 'element.style', under 'Styles'
-// output the variable by calling a function we created in 'vendor.js', taking our inputs, and targets/overrides two pieces of our index.html code
-//// outputResult(currentResult, currentDescription);
+// declare empty array
+let logEntries = [];
 
-// 2b
-// create a function, called 'add', that would add two numbers (two parameters that we would pass)
-function add(num1, num2) {
-    const result = num1 + num2;
-    //// alert("The result is " + result);  // outputs some alert ('alert' is a built-in function)
-    return result;  //returns some value (that can be stored in a variable for example)
+
+// define function to get user input
+function getUserNumberInput() {
+    return parseInt(userInput.value);  // 'userInput' (found in vendor.js) is a pointer to the actual user input, found on the html page   
 }
-// call our new 'add' function to have it executed and store it in a constant
-currentResult = add(1, 3);
-// output to calculator
-currentDescription = `We start with ${defaultResult} and now we have:`;
-outputResult(currentResult, currentDescription);
+// define function that simplifies the below functions, to output result 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDesscription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    // output these results by calling a function we created in 'vendor.js' that takes our inputs, and rights them into some (index.)html code (in <section id="results">)
+    outputResult(currentResult, calcDesscription);
+}
+
+// define function to write to log
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+// define function that would add two numbers
+function add() {
+    const enteredNumber = getUserNumberInput(); 
+    const initialResult = currentResult;  
+    //console.log('Input', enteredNumber, currentResult)  //console.log to debug and better understand code flow
+    currentResult += enteredNumber; 
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+// add functions for all buttons
+function subtract() {
+    const enteredNumber = getUserNumberInput();   
+    const initialResult = currentResult;  
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+}
+function multiply() {
+    const enteredNumber = getUserNumberInput();   
+    const initialResult = currentResult;  
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+function divide() {
+    const enteredNumber = getUserNumberInput();   
+    const initialResult = currentResult;  
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+// 'addBtn' (found in vendor.js) is a pointer to the actual add button on the html page
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+
+
+
+
+
+
